1f2f6c9b4c9d5ea26b842dad3c3c6647c78dbf07
John Meeker <john.meeker@science37.com>
2019-04-07 12:18:46 -0700

    Create base project and UsersTableViewController.

---
+++ StackOverflowApp/View Controllers/UsersTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  UsersTableViewController.h
+//  StackOverflowApp
+//
+//  Created by John Meeker on 4/7/19.
+//  Copyright © 2019 John Meeker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UsersTableViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ StackOverflowApp/View Controllers/UsersTableViewController.m
@@ -0,0 +1,22 @@
+//
+//  UsersTableViewController.m
+//  StackOverflowApp
+//
+//  Created by John Meeker on 4/7/19.
+//  Copyright © 2019 John Meeker. All rights reserved.
+//
+
+#import "UsersTableViewController.h"
+
+@interface UsersTableViewController ()
+@property (nonatomic, weak) IBOutlet UITableView *tableView;
+@end
+
+@implementation UsersTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+@end
